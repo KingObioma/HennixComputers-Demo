aeead5359eda37f15c2bfea3a9ae8814f7007f2a
king <kelvinozak0@gmail.com>
2026-02-08 23:24:48 +0100

    Initial commit  Hennix demo site

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -23,7 +23,7 @@ function ThankYouContent() {
             viewBox="0 0 24 24"
           >  <svg
             className="w-10 h-10 text-green-500"
-            fill="none"
+            fill=""
             viewBox="0 0 24"
           ></svg>
             <path
